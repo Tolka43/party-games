31131f424e82be9ced320766ab094dd3d44a2ce8
Ola Tomczyk <ola91057@gmail.com>
2022-07-31 22:51:23 +0200

    beginning v2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './landing page/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/landing page/LandingPage.js
@@ -0,0 +1,5 @@
+import './LandingPage.css'
+
+const LandingPage = () => <div className="background">hello world</div>
+
+export default LandingPage
